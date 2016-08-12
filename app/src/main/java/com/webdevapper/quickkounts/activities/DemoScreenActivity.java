f563ec33aad0d8ffeb30c2a08fae7f883dc5e640
unknown <manisht2904@gmail.com>
2016-08-12 20:10:17 +0530

    Initial commit

---
+++ app/src/main/java/com/webdevapper/quickkounts/activities/DemoScreenActivity.java
@@ -0,0 +1,15 @@
+package com.webdevapper.quickkounts.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webdevapper.quickkounts.R;
+
+public class DemoScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_screen);
+    }
+}
---
+++ app/src/main/java/com/webdevapper/quickkounts/activities/TransactionDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.webdevapper.quickkounts.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webdevapper.quickkounts.R;
+
+public class TransactionDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_details);
+    }
+}
